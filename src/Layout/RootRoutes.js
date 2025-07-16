12496e4dea13c111a613942c1415f9a2f9306a25
Matthew Lance <matthewlance55@gmail.com>
2025-07-16 11:44:00 -0400

    add root routes

---
+++ src/Layout/RootRoutes.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../home/Home";
+import { listDecks } from "../utils/api";
+import NotFound from "./NotFound";
+import Decks from "../decks/Decks";
+
+function RootRoutes() {
+  const [decks, setDecks] = useState([]);
+
+  useEffect(() => {
+    async function getDecks() {
+      try {
+        const deckList = await listDecks();
+        setDecks(deckList);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    getDecks();
+  }, [listDecks]);
+
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Home decks={decks} setDecks={setDecks} />} />
+        <Route path="/decks/*" element={<Decks decks={decks} setDecks={setDecks} />}/>
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default RootRoutes;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import RootRoutes from "./RootRoutes";
 
 function Layout() {
   return (
@@ -8,6 +9,7 @@ function Layout() {
       <Header />
       <div className="container">
         {/* TODO: Implement the screen starting here */}
+        <RootRoutes/>
         <NotFound />
       </div>
     </>
